50313186f4f5dcd4b08312656c0e89eabade4030
jwjin <jwjin@me.com>
2016-06-28 17:21:10 +0900

    add html

--- client/insertData.js
+++ client/insertData.js
@@ -3,6 +3,12 @@ Template.insertData.events(
     // button이 클릭되면 우측 함수가 실행된다
     "click #btnSend": function(evt, tmpl) {
       evt.preventDefault();
+      var user = Meteor.user();
+      console.log(user);
+      if(!user) {
+        return alert('로그인이 필요합니다');
+      }
+
     //  data를 input에서 꺼내서 디비에 저장한다
       var obj = {};
       obj.inpName = $('#inpName').val();
--- client/table.js
+++ client/table.js
@@ -5,3 +5,15 @@ Template.table.helpers(
     }
   }
 );
+
+Template.table.events(
+  {
+    "click tr": function(evt, tmpl) {
+
+      console.log('tr clicked');
+      console.log($(this)[0].inpTextarea);
+      $('#targetModal').text($(this)[0].inpTextarea);
+      //console.log($(evt).find('tr').attr('dataId'));
+    }
+  }
+);
