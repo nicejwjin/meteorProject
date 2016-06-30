bcdec6b50ec544cbcc61014500fe790b959ad8f7
jwjin <jwjin@me.com>
2016-06-30 10:34:54 +0900

    file transfer

--- client/insertData.js
+++ client/insertData.js
@@ -9,19 +9,47 @@ Template.insertData.events(
         return alert('로그인이 필요합니다');
       }
 
-    //  data를 input에서 꺼내서 디비에 저장한다
+
+
+      //data를 input에서 꺼내서 디비에 저장한다
       var obj = {};
       obj.inpName = $('#inpName').val();
       obj.inpEmail = $('#inpEmail').val();
       obj.inpPhone = $('#inpPhone').val();
       obj.inpSelect = $('#inpSelect').val();
       obj.inpTextarea = $('#inpTextarea').val();
-      Boards.insert(obj);
 
-      $('#inpName').val("");
-      $('#inpEmail').val("");
-      $('#inpPhone').val("");
-      $('#inpTextarea').val("");
+      var image = $('#inpFile').val();
+
+      if(image.length > 0) {
+        console.log('after');
+        fileObj = tmpl.find("#inpFile").files[0]
+        fileReader = new FileReader();
+        fileReader.onload = function(file) {
+          console.log('callback');
+          obj.image = file.srcElement.result;
+
+          Boards.insert(obj);
+
+          $('#inpName').val("");
+          $('#inpEmail').val("");
+          $('#inpPhone').val("");
+          $('#inpTextarea').val("");
+          $('#inpFile').val("");
+        }
+        fileReader.readAsDataURL(fileObj);
+      }
+      else {
+        Boards.insert(obj);
+
+        $('#inpName').val("");
+        $('#inpEmail').val("");
+        $('#inpPhone').val("");
+        $('#inpTextarea').val("");
+        $('#inpFile').val("");
+      }
+
+
     }
   }
 );
--- client/table.js
+++ client/table.js
@@ -9,11 +9,12 @@ Template.table.helpers(
 Template.table.events(
   {
     "click tr": function(evt, tmpl) {
-
-      console.log('tr clicked');
-      console.log($(this)[0].inpTextarea);
-      $('#targetModal').text($(this)[0].inpTextarea);
-      //console.log($(evt).find('tr').attr('dataId'));
+      Session.set('selectedData', this);
+      //console.log('tr clicked');
+      //console.log($(this)[0].inpTextarea);
+      //$('#targetModal').text($(this)[0].inpTextarea);
+      //$('#myModalLabel').text($(this)[0].inpName)
+      ////console.log($(evt).find('tr').attr('dataId'));
     }
   }
 );
